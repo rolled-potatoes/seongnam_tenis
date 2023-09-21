bb5a401d72cbb63ddee860ea3da8e3353eaebbfe
rolled-potatoes <keep4404@gmail.com>
2023-09-21 11:35:46 +0900

    feat: app place api

--- src/routes/places/v1/index.js
+++ src/routes/places/v1/index.js
@@ -1,14 +1,13 @@
 const express = require('express');
-const axios = require('axios');
 const router = express.Router();
 const { facMap } = require('@root/src/constants/facId');
 const dayjs = require('dayjs');
 const { getAvailableTimeTable } = require('@root/src/libs/isdc');
 
-router.get('', async (req, res) => {
+router.get('', async (_, res) => {
   const date = dayjs().format('YYYY-M-D');
   const jobs = Object.values(facMap).map((facId) => {
-    return getAvailableTimeTable({ id: facId, date: '2023-9-19' });
+    return getAvailableTimeTable({ id: facId, date });
   });
 
   try {
@@ -16,12 +15,10 @@ router.get('', async (req, res) => {
     const fulltiledData = datas.filter((data) => data.status === 'fulfilled');
     const data = fulltiledData.reduce((acc, cur) => {
       const { value } = cur;
-      const list = Object.entries(value).filter(
-        ([_, v]) => v.length > 0,
-      );
-      list.forEach(([key,l])=>{
-        acc[key] = l
-      })
+      const list = Object.entries(value).filter(([_, v]) => v.length > 0);
+      list.forEach(([key, l]) => {
+        acc[key] = l;
+      });
       return acc;
     }, {});
 
