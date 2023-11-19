7a77f4664c761b777f372669920af7c695b2b37e
rolled-potatoes <keep4404@gmail.com>
2023-11-19 13:25:52 +0900

    feat: api env로 변경

--- react-app/src/query-hooks/usePlaces/api.js
+++ react-app/src/query-hooks/usePlaces/api.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export function getPlaces() {
   return axios({
     method: 'GET',
-    url: 'https://p5kxyu8t53.execute-api.ap-northeast-2.amazonaws.com/default/tenis',
+    url: import.meta.env.API_URI,
   });
 }
