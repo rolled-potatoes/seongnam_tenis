4a740e3ba59c02a166703deea56c6c729f83512a
rolled-potatoes <keep4404@gmail.com>
2023-11-19 13:49:08 +0900

    fix: env path 수정

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const express = require('express');
 const path = require('path');
 const enviroment = process.env.NODE_ENV;
-const config = require(path.resolve('src', `env.${enviroment}.json`));
+const config = require(path.resolve(__dirname, `env.${enviroment}.json`));
 
 const app = express();
 
